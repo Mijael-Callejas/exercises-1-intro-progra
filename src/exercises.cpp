fe2f864cea983bd569b3018aa5c45be7d727393c
Mijael-Callejas <159574812+Mijael-Callejas@users.noreply.github.com>
2024-03-16 10:11:53 -0400

    .

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -4,16 +4,40 @@
 
 using namespace std;
 
-void exercise_1(string s1, string s2, string s3) {
-  // TODO: YOUR CODE HERE
+void exercise_1(string a, string b, string c) {
+ a = "Wibo";
+ b = "Webo"; 
+ c = "Wabu";
+ if (a < b && a < c){
+  cout << a;
+ } 
+ else if(b < a && b < c){
+  cout << b;
+ } 
+else if(c < b && c < a){
+  cout << c;
+ } 
+ } 
 }
 
 void exercise_2(double A, double B, double C) {
-  // TODO: YOUR CODE HERE
+
 }
 
 void exercise_3(int a, int b) {
-  // TODO: YOUR CODE HERE
+  cout << "Ingrese 2 numeros que sean de entre 100" << endl;
+  cin >> a;
+  cin >> b;
+  if (b == 0){
+    cout << "Imposible";
+  }
+  else{
+    int resultado = a / b;
+    cout << "El resultado es: " << resultado;
+  }
+  else {
+    cout >> "El resultado es: " >> resultado;
+  }
 }
 
 void exercise_4(double n, double a, double b, double x, double y) {
@@ -21,19 +45,64 @@ void exercise_4(double n, double a, double b, double x, double y) {
 }
 
 void exercise_5(char character) {
-  // TODO: YOUR CODE HERE
+    cin >> character;
+    int res = character;
+    cout << res << endl;
+    if(res >= 65 && res < 91){
+        cout << "lower-case alphabet" << endl;
+    }
+    else if (res >= 97 && res < 123){
+        cout << "upper-case alphabet" << endl;
+    }
+    else{
+        cout << "not an alphabet" << endl;
+    }
+
 }
 
-void exercise_6(int number) {
-  // TODO: YOUR CODE HERE
+void exercise_6(int a) {
+cout << "Ingresa tu número de la semana!" << endl;
+cin >> a;
+if(a == 1){
+ cout << "Lunes";
+}
+else if(a == 2){
+    cout << "Martes";
+}
+else if(a == 3){
+    cout << "Miercoles";
+}
+else if(a == 4){
+    cout << "Jueves";
+}
+else if(a == 5){
+    cout << "Viernes";
+}
+else if(a == 6){
+    cout << "Sabado";
+}
+else if(a == 7){
+    cout << "Domingo";
+}
+else{
+    cout << "Invalid input";
+}
 }
 
 void exercise_7(double r) {
-  // TODO: YOUR CODE HERE
+cin >> r;
+double resultado = 4 * 3.14 * (r * r);
+if (r >= 0){
+    cout << resultado << endl;
+}
+else {
+    cout << "Error: Radius cannot be negative." << endl;
+}
+
 }
 
 void exercise_8(long int seconds) {
-  // TODO: YOUR CODE HERE
+
 }
 
 string exercise_9(string s1, string s2, string s3, string s4, string s5) {
@@ -42,11 +111,21 @@ string exercise_9(string s1, string s2, string s3, string s4, string s5) {
 }
 
 int exercise_10(int a, int b) {
-  // TODO: YOUR CODE HERE
+int resa;
+int resf;
+cin >> a;
+cin >> b;
+resa = a + b;
 }
 
 string exercise_11(int number) {
-  // TODO: YOUR CODE HERE
+    cin >> number;
+if (number == 11235813){
+    cout << "Se encontro Fibonacci" << endl;
+}
+else{
+    cout << "Esto no es de Fibonacci" << endl;
+}
   return "";
 }
 
@@ -54,16 +133,35 @@ void exercise_12(string color1, int numb1,
                  string color2, int numb2,
                  string color3, int numb3,
                  string color4, int numb4) {
-  // TODO: YOUR CODE HERE
+
 }
 
 string exercise_13(int age, int years_of_experience) {
-  // TODO: YOUR CODE HERE
+cin >> age;
+cin >> years_of_experience;
+if(age >= 18 && years_of_experience >= 0 && years_of_experience < 3){
+    cout << "coordinador de proyecto" << endl;
+}
+else if(age >= 18 && years_of_experience >= 3 && years_of_experience < 5){
+    cout << "director de proyecto"  << endl;
+}
+else if(age >= 18 && years_of_experience >= 5){
+    cout << "director senior"  << endl;
+}
   return "";
 }
 
 string exercise_14(int number_of_docs) {
-  // TODO: YOUR CODE HERE
+cin >> number_of_docs; 
+if(number_of_docs == 0){
+cout << "No se encontro ningun documento" << endl;
+}
+else if(number_of_docs == 1){
+cout << "Se encontro un documento" << endl;
+}
+else{
+  cout << number_of_docs << " Documentos encontrados" << endl;
+}
   return "";
 }
 
