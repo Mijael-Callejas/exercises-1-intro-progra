8d69cf301e32c953e5744ada4883564788b9f39d
Mijael-Callejas <159574812+Mijael-Callejas@users.noreply.github.com>
2024-03-16 15:18:23 -0400

    sad

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -176,7 +176,7 @@ else if(number_of_docs == 1){
 return "Se encontro un documento";
 }
 else{
-cout << number_of_docs << "\ndocumentos encontrados" << endl;
+cout << number_of_docs << "documentos encontrados" << endl;
 }
   return "";
 }
