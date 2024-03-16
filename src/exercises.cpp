1094d8c76e10f9e72b1f87b54a491528b832be49
Mijael-Callejas <159574812+Mijael-Callejas@users.noreply.github.com>
2024-03-16 12:50:02 -0400

    asdas

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -139,7 +139,7 @@ cout << resf << endl;
 
 string exercise_11(int number) {
 if (number == 11235813){
-    cout << "Se encontro a Fibonacci" << endl;
+    cout << "" << endl;
 }
 else{
     cout << "" << endl;
