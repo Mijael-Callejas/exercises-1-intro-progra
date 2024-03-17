1145a736d53a0633d02ca6e61425bbd2a35854d4
Mijael-Callejas <159574812+Mijael-Callejas@users.noreply.github.com>
2024-03-16 21:18:09 -0400

    asass

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -174,7 +174,7 @@ string exercise_9(string s1, string s2, string s3, string s4, string s5) {
 
 int exercise_10(int a, int b) {
 if(a < 0 && b < 0){
-  return "0";
+  return 0;
 }
 else{
   int resa;
